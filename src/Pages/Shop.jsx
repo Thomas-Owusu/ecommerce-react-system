7d4f26489a67907250869485c19dde06932757d9
Thomas Owusu <thomasowusuannor@gmail.com>
2023-11-06 16:21:23 +0200

    collections

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollection from '../Components/NewCollection/NewCollection'
 
 function Shop() {
   return (
@@ -11,6 +12,7 @@ function Shop() {
       <Hero />
       <Popular />
       <Offers />
+      <NewCollection />
     </div>
   )
 }
